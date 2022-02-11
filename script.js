875d306078573c2a8c4d221485f3304c76c22ba5
Omar <omared250@gmail.com>
2022-02-11 15:31:30 +0100

    adding income and ou money functionality

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const displayMovements = function(movements) {
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>
     `;
 
@@ -89,6 +89,19 @@ const calcDisplayBalance = function(movements) {
   labelBalance.textContent = `${balance} €`
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function(movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes} €`
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} €`
+}
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function(accs) {
   accs.forEach(function(acc){
